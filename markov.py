cdebd011b8c24253a6bdfa7995b9e60e0a621524
Adel Qalieh <aqalieh95@gmail.com>
2013-11-22 00:39:52 -0500

    Initial working prototype of Markov analysis

--- markov.py
+++ markov.py
@@ -1,3 +1,27 @@
-rawOpenedFile = open("test.txt")
-readFile = rawOpenedFile.read()
-print len(readFile)
+import random
+
+def parse(filename):
+	f = open(filename)
+	readFile = f.read()
+
+	readFile = readFile.split()
+
+	markovpairs = {}
+	for i in range(len(readFile)-2):
+		markovpairs.setdefault((readFile[i],readFile[i+1]), []).append(readFile[i+2])
+
+	return markovpairs
+
+def printer(filename):
+	markovpairs = parse(filename)
+	init = random.choice(list(markovpairs.keys()))
+	acc = " ".join(init)
+
+	for i in range(30):
+		next = random.choice(markovpairs[init])
+		acc += " " + next
+		init = (init[1], next)
+
+	print(acc)
+
+printer("bible.txt")
